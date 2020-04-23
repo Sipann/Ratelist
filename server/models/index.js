241a0fd67311cb13f5c24e0b79a36b569b2e7c04
Sipann <vauguste92@gmail.com>
2020-04-23 16:38:32 +0200

    fix: db connection secrets set on environment variables

--- server/models/index.js
+++ server/models/index.js
@@ -3,21 +3,22 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const basename  = path.basename(__filename);
+const basename = path.basename(__filename);
+require('dotenv').config();
 
-const db = new Sequelize('ratelist', 'gerardsoldevila', '1234', {
-    host: 'localhost',
-    dialect: 'postgres',
-    
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    },
-  });
+const db = new Sequelize('ratelist', process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
 
-  db
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
+
+db
   .authenticate()
   .then(() => {
     console.log('** database connected **');
@@ -26,9 +27,9 @@ const db = new Sequelize('ratelist', 'gerardsoldevila', '1234', {
     console.error('! database connection error !', err);
   });
 
-  //readDirSync reads files in models folder
+//readDirSync reads files in models folder
 
-  fs
+fs
   .readdirSync(__dirname)
   .filter(file => {
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
@@ -38,6 +39,6 @@ const db = new Sequelize('ratelist', 'gerardsoldevila', '1234', {
     db[model.name] = model;
   });
 
-  db.sync(); //{force:true} to drop the tables (if changes on the models are made)
+db.sync(); //{force:true} to drop the tables (if changes on the models are made)
 
-  module.exports = db;
+module.exports = db;
