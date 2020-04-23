459275bd4acd340fc292edda98a9a896102f5429
Sipann <vauguste92@gmail.com>
2020-04-23 18:02:14 +0200

    chore: remove unused variables in client files trackItem.js and home.js

--- client/src/components/trackItem/trackItem.js
+++ client/src/components/trackItem/trackItem.js
@@ -5,17 +5,17 @@ import './trackItem.css';
 const userName = localStorage.getItem('userName')
 
 function TrackItem (props) {
-    
+
     const [color, setColor] = useState('rgb(73, 162, 218)');
-    const [buttonText, setButtonText] = useState('Rate track');
+    // const [buttonText, setButtonText] = useState('Rate track');
     const [borderColor, SetBorderColor] = useState('1px solid white')
     const [trackId, setTrackId] = useState('');
     const [rating, setRating] = useState('Submit Rating');
 
     function OnClickEvent () {
         setColor("rgb(103, 182, 109)");
-        setButtonText('Rating saved')
-        SetBorderColor('1px solid rgb(103, 182, 109)')
+        // setButtonText('Rating saved')
+        SetBorderColor('1px solid rgb(103, 182, 109)');
     }
 
     const handleChange = (e) => {
@@ -23,45 +23,45 @@ function TrackItem (props) {
         setTrackId(e.target.getAttribute('data-trackid'));
         setColor('#92a2a8')
     }
-    
+
     function handleSubmit (e) {
         e && e.preventDefault();
-        if (trackId && rating && userName) InsertRating({userName, trackId, rating});
+        if (trackId && rating && userName) InsertRating({ userName, trackId, rating });
         setTrackId('');
         setRating('Rating stored');
     }
 
-return (
-    <div>
-        <div  className='track_container' style={{border:borderColor}}>
-            <div className='album_cover'>
-                <img src={`${props.track.album.images[0].url}`} alt='album cover'/>
-            </div>
-            <div className='track_info_rating'>
-                <div className='track_details'>
-                    <p className='track_name'>{props.track.name}</p>
-                    <p className='album_name'>{props.track.album.name} - {props.track.artists[0].name}</p>
+    return (
+        <div>
+            <div className='track_container' style={{ border: borderColor }}>
+                <div className='album_cover'>
+                    <img src={`${props.track.album.images[0].url}`} alt='album cover' />
                 </div>
-                <form className='track_rating' onSubmit={handleSubmit}>
-                    <input 
-                        className='rating_selector' 
-                        type='range' 
-                        id='rating' 
-                        min='0' 
-                        max='10'
-                        data-trackid={props.track.id}
-                        onChange={handleChange}
-                    ></input>
-                    <button 
-                        style={{background:color}} 
-                        type='submit' 
-                        className='rate_button' 
-                        onClick={()=>{OnClickEvent()}}>{rating}</button>
-                </form>
+                <div className='track_info_rating'>
+                    <div className='track_details'>
+                        <p className='track_name'>{props.track.name}</p>
+                        <p className='album_name'>{props.track.album.name} - {props.track.artists[0].name}</p>
+                    </div>
+                    <form className='track_rating' onSubmit={handleSubmit}>
+                        <input
+                            className='rating_selector'
+                            type='range'
+                            id='rating'
+                            min='0'
+                            max='10'
+                            data-trackid={props.track.id}
+                            onChange={handleChange}
+                        ></input>
+                        <button
+                            style={{ background: color }}
+                            type='submit'
+                            className='rate_button'
+                            onClick={() => { OnClickEvent() }}>{rating}</button>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
-)
+    )
 }
 
 export default TrackItem;
--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -6,7 +6,6 @@ import './home.css';
 
 function HomePage () {
 
-    const [trackIdList, setTrackIdList] = useState([]);
     const [trackRatings, setTrackRatings] = useState([]);
     const [rateList, setRateList] = useState(null);
 
@@ -19,29 +18,28 @@ function HomePage () {
         res.forEach((track) => {
             trackIds.push(track.trackId)
             trackRatings.push(track.rating)
-        })  
-        setTrackIdList(trackIds); //latest rated tracks will be displayed first.
+        })
         setTrackRatings(trackRatings); //reversing ratings to match reversed trackIds array.
-        
+
         const spotifyTrackList = await getTracks(trackIds);
-        for (let i = 0; i < trackRatings.length; i++) { 
+        for (let i = 0; i < trackRatings.length; i++) {
             spotifyTrackList.tracks[i].rating = trackRatings[i]; //insert track rating to each track on spotify res obj.
         }
-        
+
         setRateList(spotifyTrackList);
     }
-    
+
     useEffect(() => {
         getTrackIds()
-    },[]);
+    }, []);
 
     return (
         <div className="home">
             <header className='home_header_container'>
                 <nav className='nav_container'>
-                    <a className='myRatings_button_' href="/home">My ratings</a> 
-                    <a className='searchRate_button_' href="/searchRate">Search & Rate</a> 
-                    <a className='soulmates_button_' href="">Soulmates</a> 
+                    <a className='myRatings_button_' href="/home">My ratings</a>
+                    <a className='searchRate_button_' href="/searchRate">Search & Rate</a>
+                    <a className='soulmates_button_' href="">Soulmates</a>
                 </nav>
             </header>
             <div className='ratings_container'>
