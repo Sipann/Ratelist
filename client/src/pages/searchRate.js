3d7418783c3cb26d10de04d2a403fb5ca3cf7189
Sipann <vauguste92@gmail.com>
2020-04-23 18:52:09 +0200

    refactor: replace useEffect hook by direct invocation in searchRate.js

--- client/src/pages/searchRate.js
+++ client/src/pages/searchRate.js
@@ -6,34 +6,34 @@ import './searchRate.css';
 
 function SearchRate () {
 
-    const [tracks, setTracks] = useState(null);
+  const [tracks, setTracks] = useState(null);
 
-    async function getTracks (input) {
-        const res = await searchTracks(input);
-        setTracks(res);
-    }
+  async function getTracks (input) {
+    const res = await searchTracks(input);
+    setTracks(res);
+  }
 
-    useEffect(() => {
-        getTracks()
-    },[]);
+  useEffect(() => {
+    getTracks()
+  }, []);
 
-    return (
-        <div className="searchRate">
-            <header className='searchRate_header_container'>
-                <nav className='nav_container'>
-                    <a className='my_ratings_button' href="/home">My ratings</a> 
-                    <a className='searchRate_button' href="/searchRate">Search & Rate</a> 
-                    <a className='soulmates_button' href=''>Soulmates</a> 
-                </nav>
-                <div className='search_bar'>
-                    <SearchBar className='search_input' getTracks={getTracks}/>
-                </div>
-            </header>
-            <div className='tracks'>
-                {tracks && <TrackList tracks={tracks} />}
-            </div>
+  return (
+    <div className="searchRate">
+      <header className='searchRate_header_container'>
+        <nav className='nav_container'>
+          <a className='my_ratings_button' href="/home">My ratings</a>
+          <a className='searchRate_button' href="/searchRate">Search & Rate</a>
+          <a className='soulmates_button' href="/soulmates">Soulmates</a>
+        </nav>
+        <div className='search_bar'>
+          <SearchBar className='search_input' getTracks={getTracks} />
         </div>
-    );
+      </header>
+      <div className='tracks'>
+        {tracks && <TrackList tracks={tracks} />}
+      </div>
+    </div>
+  );
 }
 
 export default SearchRate
