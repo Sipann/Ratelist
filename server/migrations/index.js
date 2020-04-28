77cf1fe5a2e3a0b54cecad30218a221b5c56c735
Sipann <vauguste92@gmail.com>
2020-04-28 15:15:48 +0200

    test: add /ratings testing

--- server/migrations/index.js
+++ server/migrations/index.js
@@ -1,12 +1,8 @@
 module.exports = {
-  up: (queryInterface) => {
-    return queryInterface.bulkInsert('"Ratings"', [{
-      userName: 'yy6sake8jqanxinb3yoxa44xw',
-      trackId: '6sCYUYJkkqP5wJG4ccLGuA',
-      rating: 10,
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }]);
+  up: (_) => {
+    return new Promise((resolve, reject) => {
+      resolve(true);
+    });
   },
   down: (queryInterface) => {
     return queryInterface.bulkDelete('"Ratings"', null, {});
--- server/tests/routes.test.js
+++ server/tests/routes.test.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = require('../router');
 const cors = require('cors');
 
+
 let server, agent;
 
 beforeEach((done) => {
@@ -18,6 +19,7 @@ beforeEach((done) => {
     agent = request.agent(server);
     done();
   });
+
 });
 
 afterEach((done) => {
@@ -27,12 +29,67 @@ afterEach((done) => {
 
 describe('/ratings routes', () => {
 
+  beforeEach(async () => {
+
+    const fakeRating1 = {
+      userName: 'yy6sake8jqanxinb3yoxa44xw',
+      trackId: '1HFiThfrSDT9byxv6cw8Tm',
+      rating: 10,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+    const fakeRating2 = {
+      userName: 'yy6sake8jqanxinb3yoxa44xw',
+      trackId: '1h2xVEoJORqrg71HocgqXd',
+      rating: 7,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+    const fakeRating3 = {
+      userName: 'yy6sake8jqanxinb3yoxfalse',
+      trackId: '1h2xVEoJORqrg71Hofalse',
+      rating: 5,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+
+    await agent.post('/ratings').send(fakeRating1);
+    await agent.post('/ratings').send(fakeRating2);
+    await agent.post('/ratings').send(fakeRating3);
+
+  });
+
   it('should get all ratings', async () => {
     const res = await agent.get('/ratings');
     expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveLength(3);
     res.body.forEach(item => {
       expect(item).toHaveProperty('rating');
     });
+  });
+
+  it('should get all ratings for a user', async () => {
+    const username = 'yy6sake8jqanxinb3yoxa44xw';
+    const res = await agent.get(`/ratings/${username}`);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveLength(2);
+  });
+
+  it('should create a rating', async () => {
+    const newRating = {
+      userName: 'yy6sake8jqanxinb3yoxfalse',
+      trackId: '6sCYUYJkkqP5wJG4ccLGuA',
+      rating: 10,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    };
+    const res = await agent
+      .post('/ratings')
+      .send(newRating);
+    expect(res.statusCode).toEqual(201);
+    expect(res.body).toHaveProperty('userName');
+    expect(res.body).toHaveProperty('trackId');
+    expect(res.body).toHaveProperty('rating');
   });
 
 
