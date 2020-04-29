7d825ef62685194319f1c84f2747e820cd54915d
Petr Penicka <petr.penicka@gmail.com>
2020-04-29 16:04:57 +0200

    feat: separate navigation to NavigationBar component

---
+++ client/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './NavigationBar.css';
+import { Link } from "react-router-dom";
+
+function NavigationBar (props) {
+
+  return (
+    <nav className='nav-container'>
+      <Link className='btn-nav btn-my-ratings' to="/home">My ratings</Link>
+      <Link className='btn-nav' to="/searchRate">Search & Rate</Link>
+      <Link className='btn-nav' to="/soulmates">Soulmates</Link>
+    </nav>
+  )
+}
+
+export default NavigationBar;
